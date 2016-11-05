afd100c5c5290d64c18bb968dc284fbbbe88d510
Talon Hughes <talon.hughes@gmail.com>
2016-11-05 10:35:35 -0600

    template changes

--- js/app.js
+++ js/app.js
@@ -16,12 +16,12 @@ app.config(function($urlRouterProvider, $stateProvider) {
     //#2 Landing Page(new users' profile page) - User is taken here when they submit the initial form on the Home page.
     .state('landing', {
             url: '/landing',
-            templateUrl: '/views/templates/landing.html'
+            templateUrl: '/views/landing.html'
         })
         //#3 Search for Friends - User can access this page only after they submit the initial form on the Home Page.
         .state('friend-search', {
             url: '/friend-search',
-            templateUrl: '/views/templates/friend-search.html',
+            templateUrl: '/views/friend-search.html',
             controller: 'friendSearchCtrl',
             resolve: {
                 checkUser: function(mainSvrc, $state) {
@@ -38,17 +38,17 @@ app.config(function($urlRouterProvider, $stateProvider) {
         //#4 Upadte Profile Information - User can access this as soon as they submit the initial form on the Home page.
         .state('update', {
             url: '/update',
-            templateUrl: 'views/templates/update.html',
+            templateUrl: 'views/update.html',
         })
         //#5 Other users' profile page. User is taken here after clicking button on image overlay on the friend-search page.
         .state('friend-profile', {
             url: '/friend-profile/:id',
-            templateUrl: 'views/templates/friend-profile.html',
+            templateUrl: 'views/friend-profile.html',
             controller: 'friendProfileCtrl'
         })
         //#6 New users' friends list. Only can access after they have added a friend.
         .state('friends', {
             url: '/friends',
-            templateUrl: '/views/templates/friends.html',
+            templateUrl: '/views/friends.html',
         });
 });
